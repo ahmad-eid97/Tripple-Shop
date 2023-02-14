0fa1561ec964497ae9e818324f11b999ec12f778
ahmad-eid97 <civil.ahmad.eid@gmail.com>
2023-02-14 17:37:43 +0200

    fix i18n

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,5 +2,7 @@ module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['ar', 'en'],
+    localePath: typeof window === 'undefined' ? require('path').resolve('./public/locales') : '/locales',
+    reloadOnPrerender: process.env.NODE_ENV === 'development'
   },
 }
